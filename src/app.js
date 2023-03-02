17990716336cde6ea54184f228e0d6012263d5f1
Andó Attila <andoattila698@gmail.com>
2023-03-02 08:54:49 +0100

    first

--- src/app.js
+++ src/app.js
@@ -8,3 +8,12 @@
 * Licenc: GNU GPL
 * Nem vállalunk felelősséget a rossz jegyekért
 */
+const ulElem=document.querySelector('#ulElem');
+var termekek=['alma','körte','barack','sajt'];
+termekek.forEach(()=>{
+    //console.log(termekek)
+    let li=document.createElement('li');
+    li.textContent=termekek;
+    li.classList='list-group-item';
+    ulElem.append(li);
+})
