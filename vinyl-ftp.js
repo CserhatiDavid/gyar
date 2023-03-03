6f4408bed67805cb1ceecdff9ea9e6b079f0fde7
CserhatiDavid <csacsd@gmail.com>
2023-03-03 10:28:11 +0100

    FTP scripted

--- vinyl-ftp.js
+++ vinyl-ftp.js
@@ -1,10 +1,16 @@
 var fs = require( 'vinyl-fs' );
 var ftp = require( 'vinyl-ftp' );
- 
+var reader=require('properties-reader');
+var config=reader('config/config.properties');
+
+const host=config.get('hosting.host');
+const user=config.get('hosting.user');
+const pass=config.get('hosting.password');
+
 var conn = new ftp( {
-    host: 'ftpupload.net',
-    user: 'valaki',
-    password: 'titok',
+    host: host,
+    user: user,
+    password: pass,
     parallel: 10,
     secure: true,
     secureOptions: {rejectUnauthorized: false}
